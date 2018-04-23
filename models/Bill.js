192b4f903274cf5b857919eedcb813cfb7bfacac
Gabe <gperry22@gmail.com>
2018-04-23 16:57:34 -0400

    Bill Schema

--- models/Bill.js
+++ models/Bill.js
@@ -3,17 +3,29 @@ var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new NoteSchema object
+// Using the Schema constructor, create a new BillSchema object
 // This is similar to a Sequelize model
-var NoteSchema = new Schema({
-    // `title` is of type String
-    // title: String,
-    // `body` is of type String
-    body: String
+var BillSchema = new Schema({
+
+    billName: {
+        type: String,
+        required: true
+    },
+
+    billAmount: {
+        type: Number,
+        required: true
+    },
+    
+    billStatic: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Note = mongoose.model("Note", NoteSchema);
+var Bill = mongoose.model("Bill", BillSchema);
 
-// Export the Note model
-module.exports = Note;
+// Export the Bill model
+module.exports = Bill;

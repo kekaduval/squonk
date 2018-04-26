54ea734819a9bdb7cb2e82fbafbde04f83043cbb
Nathan <nathanalston@gmail.com>
2018-04-26 01:21:25 -0400

    Test

--- client/src/components/BudgetBar/BudgetBar.jsx
+++ client/src/components/BudgetBar/BudgetBar.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import "./BudgetBar.css"
+var Select = require('react-select');
 
 class BudgetBar extends React.Component{
+  constructor(){
+    super();
 
+  this.state = {
+    users: [],
+  }
+};
+
+  componentDidMount() {
+    this.loadBudgets();
+  }
+
+  // Loads all books  and sets them to this.state.books
+  loadBudgets = () => {
+    API.getBudgets()
+      .then(res =>
+        this.setState({ users: res.data })
+      )
+      .catch(err => console.log(err));
+  };
+  
 render(){
+  const image = "image"
   return (
+    <React.Fragment>
     <div>
-    <h1>Budget Bar</h1>
+    <ul className="dropdown-menu" id="dropdown">
+    <li>{image}</li>
+    </ul>
     </div>
+    </React.Fragment>
   )
 }
 }

1dcb12d2877e105c19941581ebf855cf5d9fdc97
Nathan <nathanalston@gmail.com>
2018-04-25 21:10:40 -0400

    Add button no css

---
+++ client/src/components/AddButton/AddButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./AddButton.css"
+
+const AddButton = props => (
+  <button className="addButton" title="Add a Budget">+
+  </button>
+
+)
+
+export default AddButton;

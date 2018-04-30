cc6cb626b079e1789c863c5affe4736cb0467451
Gabe <gperry22@gmail.com>
2018-04-30 18:56:23 -0400

    updates

--- client/src/components/BillDisplayItem/BillsDisplayItem.jsx
+++ client/src/components/BillDisplayItem/BillsDisplayItem.jsx
@@ -3,11 +3,15 @@ import "./BillsDisplayItem.css"
 
 const BillDisplayItem = props => {
 
+  const style = {
+    color: props.bill.billActualAmount > props.bill.billPlannedAmount ? 'red' : 'green'
+  };
+
   return (
     <div className='row rowMargin'>
-      <div className='col-md-2 text-center rowBG'><h5>{props.bill.billName}</h5></div>
-      <div className='col-md-2 text-center rowBG'><h5>${props.bill.billPlannedAmount.toFixed(2)}</h5></div>
-      <div className='col-md-2 text-center rowBG'><h5>${props.bill.billActualAmount.toFixed(2)}</h5></div>
+      <div className='col-md-2 text-center rowBG'><h5 >{props.bill.billName}</h5></div>
+      <div className='col-md-2 text-center rowBG'><h5 style={style}>${props.bill.billPlannedAmount.toFixed(2)}</h5></div>
+      <div className='col-md-2 text-center rowBG'><h5 style={style}>${props.bill.billActualAmount.toFixed(2)}</h5></div>
       <div className='col-md-1 text-center rowBG'><h5>{props.bill.billStatic}</h5></div>
       <div className='col-md-2 text-center rowBG'><h5>Edit/Delete</h5></div>
     </div>

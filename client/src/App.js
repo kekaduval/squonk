594daa77037fe86e11fc179ec383304edc6d6fc0
Gabe <gperry22@gmail.com>
2018-05-05 14:15:46 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Budgets from "./pages/Budgets";
-import LoginPage from './components/Auth';
 
 import './App.css';
 
 const App = () => (
   <React.Fragment>
-
     <Budgets />
   </React.Fragment>
 );

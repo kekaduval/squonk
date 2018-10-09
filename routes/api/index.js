1ae1ddbbcf297e71409696a010093804ed4608bb
Gabe <gperry22@gmail.com>
2018-10-09 00:27:04 -0400

    fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,11 @@
-const router = require("express").Router();
-const userRoutes = require("./user");
-const budgetRoutes = require("./budget");
-const billRoutes = require("./bill");
+const router = require('express').Router();
+const userRoutes = require('./user');
+const budgetRoutes = require('./budget');
+const billRoutes = require('./bill');
 
-// now its /api/budgets
-router.use("/user", userRoutes);
-router.use("/budget", budgetRoutes);
-router.use("/bill", billRoutes);
-
+// now its /api/budgeter
+router.use('/user', userRoutes);
+router.use('/budget', budgetRoutes);
+router.use('/bill', billRoutes);
 
 module.exports = router;
